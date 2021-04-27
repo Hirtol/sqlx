a80409071404dabc38b071f637c16b9ff5e3a474
Valentijn Hol <valentijnhol@hotmail.nl>
2021-04-27 17:11:03 +0200

    Ignore missing in migrations

--- sqlx-macros/src/migrate.rs
+++ sqlx-macros/src/migrate.rs
@@ -106,7 +106,7 @@ pub(crate) fn expand_migrator_from_dir(dir: LitStr) -> crate::Result<TokenStream
             migrations: ::std::borrow::Cow::Borrowed(&[
                 #(#migrations),*
             ]),
-            ignore_missing: false,
+            ignore_missing: true,
         }
     })
 }
